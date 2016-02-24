ad4d609262d6ed797640e5770b3200c0a95977d0
scott-linder <scott.b.linder@wmich.edu>
2016-02-24 11:28:31 -0500

    Correct t2

--- src/getRoots/t2.c
+++ src/getRoots/t2.c
@@ -33,31 +33,7 @@ int main()
     double sqrtd;		// scratch for sqrt(d)
 
 
-    // initialize the unit testing framework
     cunit_init();
-
-    //  A single test using the mock object for qsolve_sqrt()
-    //  Should have x1 = 3.0 and x2 = 1.0, except for roundoff
-    //   We expect  d =  b^2 - 4*a*c =  4.0 and
-    //              sqrt(d) returned should be 2.0
-    //  In this special case we will ytry to have the error actually == 0.0.,
-    //  both in the qsove_sqrt(I) mock object and for the returned roots!
-    //  This is not often the case.
-    setup_mock_sqrt(4.0, 2.0, 0.0);
-
-    // This looks like a check of qsolve_sqart()
-    coefs.a = 1.0;
-    coefs.b = -4.0;
-    coefs.c = 3.0;
-    num = numRoots(coefs);
-    ret = getRoots(coefs, num, &roots);
-    assert_eq("ret", ret, 2);
-    assert_feq("x1", roots.x1, 3.0);
-    assert_feq("x2", roots.x2, 1.0);
-    ret = check_mock_sqrt(&count, &x);
-    snprintf(str, 99, "count ret = %d x =%20.61e", count, x);
-    assert_eq(str, ret, 1);
-
 
     //
     // A "good" unit test, need to allow for round off!
@@ -76,7 +52,7 @@ int main()
     setup_mock_sqrt(d, sqrtd, cunit_dmacheps * 2.0 * d);
     num = numRoots(coefs);
     ret = getRoots(coefs, num, &roots);
-    assert_eq("ret", ret, 2);
+    assert_eq("ret", ret, 0);
     assert_feqrerr("x1", roots.x1, x2, 2.0 * cunit_dmacheps * 3.3);
     assert_feqrerr("x2", roots.x2, x1, 2.0 * cunit_dmacheps * 3.1);
     ret = check_mock_sqrt(&count, &x);
