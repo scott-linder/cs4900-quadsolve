f6d5cb0dc6eadde002973b76517cb020798d62c4
scott-linder <scott.b.linder@wmich.edu>
2016-02-24 11:12:40 -0500

    First getRoots unit test (t1), requiring cunit

---
+++ src/cunit/cunit.c
@@ -0,0 +1,24 @@
+// cunit.c
+// The global constants for cunit and cunit_init(),
+// which should be called befoer using the unit test 
+// macros in cunit.h
+
+#include <stdio.h>
+#include "cunit.h"
+
+FILE *cunit_log;
+double cunit_dmacheps;
+
+int cunit_init()
+{
+    double dm;
+    double dmacheps = 0.5;
+
+    cunit_log = stderr;
+
+    while ((1.0 + (dm = dmacheps / 2.0)) != 1.0) {
+	dmacheps = dm;
+    }
+    cunit_dmacheps = dmacheps;
+    return 0;
+}
---
+++ src/sqrt/mock_sqrt.h
@@ -0,0 +1,3 @@
+void mock_setup_qsolve_sqrt(double, double, double);
+int mock_check_qsolve_sqrt(int *, double *);
+void mock_teardown_qsolve_sqrt();
