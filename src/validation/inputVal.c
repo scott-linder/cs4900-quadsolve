db7b383f8238ee02dab237b57f9efd237e317f4c
justin lanyon <justin.j.lanyon@wmich.edu>
2016-02-28 18:03:22 -0500

    fixed validation added unit test

--- src/validation/inputVal.c
+++ src/validation/inputVal.c
@@ -3,7 +3,7 @@
 /*returns 1 if input is good, returns 0 if input is bad*/
 int inputVal(Coef *coef, char * input){
 	int retVal =0;
-	if(sscanf(input,"%lf %lf %lf",&coef->a,&coef->b,&coef->c)>0){
+	if(sscanf(input,"%lf %lf %lf",&coef->a,&coef->b,&coef->c)==3){
 		retVal =1;
 	}else{
 		retVal =0;
---
+++ src/validation/t2.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "../quad_types.h"
+#include "../cunit/cunit.h"
+#include "inputVal.h"
+
+int main()
+{
+	Coef coefs;
+	int ret;
+	char * input = "1 a 1";
+	cunit_init();
+	ret = inputVal(&coefs,input);
+	assert_eq("ret",ret,0);
+}
